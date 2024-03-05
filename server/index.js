a04e8e603a09c4941f725f5405912d4bc815ae12
Anil Kumar Vinnakoti <anil.vinnakoti@gmail.com>
2024-03-05 15:21:59 +0530

    [#5]: trail CORS error

--- server/index.js
+++ server/index.js
@@ -11,18 +11,25 @@ dotenv.config(); // add .env file path
 connectDB();
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Change this to the specific origin of your React app in production
+  origin: "http://localhost:3000", // Allow requests from localhost:3000
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  // preflightContinue: false,
-  // optionsSuccessStatus: 204,
-  // allowedHeaders: "Content-Type, Authorization", // Add any additional headers you need
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: "Content-Type, Authorization", // Add any additional headers you need
   // credentials: true, // Set this to true if your client sends credentials (e.g., cookies)
 };
 
 app.use(cors(corsOptions));
+// Handle CORS preflight requests for all routes
+app.options("*", cors(corsOptions));
 
 app.use(
   "/graphql",
+  (req, res, next) => {
+    // Set Access-Control-Allow-Origin header
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    next();
+  },
   graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development",
